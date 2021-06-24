3ff67496dad2077d60fc43d4bfc25aa73023e573
Alam <arshe.alam@avrioc.com>
2021-06-24 13:21:13 +0400

    yml and mongo connection updated

--- node-login-signup/lib/maindb.js
+++ node-login-signup/lib/maindb.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-mongoose.connect('mongodb://localhost:27017/demo');
+mongoose.connect('mongodb://mongo:27017/demo');
 
 mongoose.Promise = global.Promise;
 
