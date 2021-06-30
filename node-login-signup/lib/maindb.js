ea8bfbaa3280ae0f5ebfa701f5cf07e149e31b9b
Alam <arshe.alam@avrioc.com>
2021-06-30 15:46:45 +0400

    docker file changes

--- node-login-signup/lib/maindb.js
+++ node-login-signup/lib/maindb.js
@@ -9,12 +9,19 @@ mongoose.Promise = global.Promise;
 
 let db = mongoose.connection;
 
+// Retry connection
+const connectWithRetry = () => {
+    console.log('MongoDB connection with retry')
+    mongoose.connect('mongodb://db:27017/demo');
+}
+
 db.on('connected', function() {
     console.log('[Server]', 'Connection with MongoDB installed');
 });
 
 db.on('error', function(err) {
     console.log('++++++ [error] ++++++', err);
+    setTimeout(connectWithRetry, 5000)
 });
 
 module.exports = db;
